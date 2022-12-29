e1a20974d191403b5edaf33023ed4d10e86b8bb8
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2022-12-29 00:07:07 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-console.log(process.argv);
+import readline from "node:readline";
 
-const flags = [];
-
-process.argv.forEach((arg) => {
-  if(/^-/.test(arg)) {
-    flags.push(arg.replaceAll("-", ""));
-  }
+const rl = readline.createInterface({
+  terminal: true,
+  input: process.stdin,
+  output: process.stdout,
 });
 
-console.log(flags);
+console.log("What is your name?");
 
-if(flags.includes("a") || flags.includes("add")) {
-  console.log("add some values");
-} else {
-  console.log("Do some work");
-}
+let inmput = "";
 
-console.log(process.argv)
+rl.input.on("keypress", (event, rl) => {
+  console.log(event, rl)
+})
+
+console.log(process.argv);
+
+// const flags = [];
+
+// process.argv.forEach((arg) => {
+//   if(/^-/.test(arg)) {
+//     flags.push(arg.replaceAll("-", ""));
+//   }
+// });
+
+// console.log(flags);
+
+// if(flags.includes("a") || flags.includes("add")) {
+//   console.log("add some values");
+// } else {
+//   console.log("Do some work");
+// }
+
+// console.log(process.argv)
