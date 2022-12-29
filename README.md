f6b46248bd38bc3c5dda7982bf520e032ec0ba23
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2022-12-28 23:55:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# quizme-cli
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-2cemqp)
---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http')
+
+module.exports = {
+  name: "Andrew"
+};
+
+// ECMAScript MOdules
+import http from "http";
+
+export const name = "Andrew";
